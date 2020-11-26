40c51dc7fc6eafabd429f531606139546fd686c2
KuyaShawn <EashuneAbenojar@hotmail.com>
2020-11-26 01:35:59 -0800

    Portfolio/Resume
    Still work in progress. But this is Portfolio/Resume website to show case my work and skills to potential employers.

---
+++ My_Portfolio-Resume/includes/footer.php
@@ -0,0 +1,38 @@
+<?php
+// This is the HTML for the page footer.
+?>
+<footer>
+    <aside class="footer-container">
+        <aside class="contactBox">
+            <h2>Contact</h2>
+            <ul>
+                <li>Personal Email: <a href="mailto:EashuneAbenojar@hotmail.com">EashuneAbenojar@hotmail.com</a> </li>
+                <li>School Email: <a href="mailto:eabenojar@mail.greenriver.edu">eabenojar@mail.greenriver.edu</a></li>
+                <li>Facebook: Eashune Abenojar</li>
+                <li>Instagram: @Kuya_Shawn</li>
+            </ul>
+        </aside>
+        <aside class="indexBox">
+            <h2>Index</h2>
+            <nav>
+                <a href="index.php#aboutMe">About Me</a>
+                <a href="index.php#education">Education</a>
+                <a href="index.php#csProjects">CS Projects</a>
+                <a href="index2.php#workHistory">Work History</a>
+                <a href="index2.php#personalStatement">Personal Statement</a>
+                <a href="index2.php#personalSkills">Personal Skills</a>
+                <a href="index2.php#personalGoals">Personal Goals</a>
+            </nav>
+        </aside>
+    </aside>
+    <aside class="copyrightBox">
+        <p>&copy; <?php echo date("Y"); ?> Eashune Abenojar</p>
+    </aside>
+</footer>
+<!-- Background animation dots -->
+<ul id="background">
+</ul>
+<script src="javascript/background.js"></script>
+</body>
+
+</html>
---
+++ My_Portfolio-Resume/includes/nav.php
@@ -0,0 +1,16 @@
+<?php
+// This is the HTML for website navigation. 
+?>
+<nav class="navigation">
+    <label for="toggle">&#9776;</label>
+    <input type="checkbox" id="toggle"/>
+        <aside class="menu">
+            <a href="index.php#aboutMe">About Me</a>
+            <a href="index.php#education">Education</a>
+            <a href="index.php#csProjects">CS Projects</a>
+            <a href="index2.php#workHistory">Work History</a>
+            <a href="index2.php#personalStatement">Personal Statement</a>
+            <a href="index2.php#personalSkills">Personal Skills</a>
+            <a href="index2.php#personalGoals">Personal Goals</a>
+        </aside>
+</nav>
