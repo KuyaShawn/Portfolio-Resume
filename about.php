25506003c383a34de19e8cf71823a62fb9432d31
KuyaShawn <EashuneAbenojar@hotmail.com>
2021-08-26 03:37:37 -0700

    Added about page and updated it.

---
+++ about.php
@@ -0,0 +1,123 @@
+<?php
+// This puts the beginning part of the HTML file into the page
+include "includes/header.php";
+?>
+    <section class="bg-light">
+        <section class="text-dark container pt-4 mb-5">
+            <h1 class="text-center text-dark ">About Me</h1>
+            <p class="text-dark ">
+                Greetings! my name is Eashune Abenojar. And thank you for viewing my website I hope
+                you enjoy it!
+                <br><br>
+                I am a Junior majoring in Computer Science at Green River College. From a young age, I've had a strong
+                interest in programming and I've found it to be a great way to solve many problems. Programming has
+                always been a way for me to let my creativity flow, and it is something I am very passionate about.
+                <br><br>
+                My academic and professional experiences have enabled me to develop valuable skills, and I would like to
+                augment my implementation of such skills by working in a formal environment, preferably in the areas of
+                back-end development or full-stack development.
+                <br><br>
+                As a quick learner ready to acclimate to any environment and someone who looks forward to challenges, I
+                hope to leverage my analytical and creative skills to work on meaningful projects and to gain new
+                experiences. Feel free to connect with me or contact me through email at EashuneAbenojar@hotmail.com
+                regarding any inquiries or opportunities.
+            </p>
+        </section>
+        <hr>
+        <h1 class="text-center text-dark">Portfolio</h1>
+        <h3 class="text-center mt-4">Timeline/Education</h3>
+        <aside class="timeline-container">
+            <aside class="timeLine">
+                <ul>
+                    <li>
+                        <aside class="timeLine-content">
+                            <h3 class="date text-dark">2017</h3>
+                            <h2>How it all started</h2>
+                            <p>
+                                After graduating, I didn't know what i've wanted to do after
+                                High school. But after taking an HTML course at Highline College. I've researched the
+                                Computer Science major, and that's when I became interested in pursuing this major.
+                            </p>
+                        </aside>
+                    </li>
+                    <li>
+                        <aside class="timeLine-content">
+                            <h3 class="date text-dark">2020</h3>
+                            <h2>Graduating Highline College with an AS</h2>
+                            <p>
+                                During my year in Highline, I grew to learn the basics of HTML, CSS, JavaScript, SQL,
+                                and Java. And with the help of these projects and classes, I've become more accustomed
+                                to wanting to learn more and get better at coding with multiple languages. And besides. those CS
+                                classes it helped me gain a few minor projects to add to my collection of repos in
+                                GitHub.
+                            </p>
+                        </aside>
+                    </li>
+                    <li>
+                        <aside class="timeLine-content">
+                            <h3 class="date text-dark">2021</h3>
+                            <h2>Introduction to HTML & JavaScript</h2>
+                            <p>
+                                During This year, I took an HTML &amp; JavaScript class, as I already
+                                knew a small portion of HTML, still had lots of things to learn. the
+                                same goes for JavaScript, even though JavaScript had a few similarities
+                                to Java, know how to implement them into HTML was important. But overall
+                                I&apos;ve enjoyed my learning how to code HTML and Javascript as I
+                                learned how to create a few functional websites using both those
+                                languages. it wasn&apos;t at all professionally done, but it worked.
+                            </p>
+                        </aside>
+                    </li>
+                    <li>
+                        <aside class="timeLine-content">
+                            <h3 class="date text-dark">2022</h3>
+                            <h2>Graduation</h2>
+                            <p>
+                                2022 will be the year I will graduate with my Bachelor degree in Information
+                                Technology-Software Development, with a certificate of IT Web Application Development.
+                            </p>
+                        </aside>
+                    </li>
+                </ul>
+            </aside>
+        </aside>
+        <hr>
+        <h1 class="pt-3 text-center ">Projects</h1>
+        <!-- add box styles -->
+        <aside class="container">
+            <aside class="mt-4">
+                <h4>First Website</h4>
+                <p>
+                    For my first project I created my first website using the basics of HTML,
+                    CSS, and JavaScript. It isn&apos;t a great website, but it was a start of
+                    something new, which I then got very interested in. I put a lot of effort
+                    into this as a newcomer to programming, but I&apos;m happy how it sorta
+                    turned out
+                </p>
+            </aside>
+            <aside class="mt-4">
+                <h4>First Java animated art</h4>
+                <p>
+                    For my second project, for Java 1 I&apos;ve created an art project that used
+                    an &quot;acm&quot; file for graphic/animation designs. This was an overall
+                    fun project for Java 1 and I plan to create more as a little practice
+                    project..
+                </p>
+            </aside>
+            <aside class="mt-4 pb-4">
+                <h4>First GUI</h4>
+                <p>
+                    For my Third project, for Java 2 I&apos;ve created a basic GUI file that
+                    takes in order list. by letting the user choose what type of drink they
+                    wanted and which assortment they wanted to add into their drink, this GUI
+                    had a working quantity inputter, cost calculation, etc.&nbsp;
+                </p>
+            </aside>
+        </aside>
+    </section>
+
+
+<?php
+// This puts the HTML for the footer into the page.
+include "includes/footer.php";
+?>
---
+++ more_about.php
@@ -0,0 +1,189 @@
+<?php
+// This section defines the variables that will be used to
+// insert text into the top of the page..
+$title = "Eashune Abenojar";
+$description = "Resume/Portfolio of important information of myself";
+$author = "C SCI 202 Eashune Abenojar";
+$mainheading = "Eashune Abenojar";
+
+// This puts the beginning part of the HTML file into the page
+include "includes/header.php";
+?>
+<!-- Your HTML for the main content of the page will go here. -->
+<section class="secondPage">
+    <h2>More About Me</h2>
+</section>
+
+<aside>
+    <section class="work_Container">
+        <h3 id="workHistory">Work History</h3>
+        <!-- add info below -->
+        <h4>Food Service Worker</h4>
+        <p>
+            My role in the foodservice is to help bring food to the designated areas
+            within the hospital. While also working as a team to deliver the food to the
+            patients. Also, it&apos;s my job to help get the food prepared and organized
+            for the patient. And some time to time I will help within the dish room.
+        </p>
+
+        <h4>Customer Site Warehouse Support</h4>
+        <p>
+            During my whole time working at Fastenal as a Part-Timer. I was able to
+            learn plenty of things, such as taking deliveries, making orders, make
+            branch transfers, receive in orders/transfers, and learning how to
+            communicate as a team while also learning how to become a leader when
+            someone doesn&apos;t know what to do. For deliveries my job was to take in
+            the orders I&apos;ve or someone else picked the day before. And then I go
+            out and deliver the parts such as gloves, wrenches, nuts, bolts, and screws
+            to the company that wanted those items. Sometimes my manager tells me to
+            receive and it&rsquo;s a pretty easy job, as all I do is scan the parts that
+            have been sent by the company. Branch transfers, which is picking items and
+            making custom shipping labels for other Fastenal companies to sell to their
+            customers. There is a lot of heavy equipment &amp; boxes that are needed to
+            be moved around within the warehouse, so dealing with heavy things is very
+            common for me to work around with.
+        </p>
+    </section>
+
+    <section>
+        <h3 id="personalStatement">Personal Statement</h3>
+        <p class="statment_P">
+            Essay Prompt:
+        </p>
+        <p class="statment_P">
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;
+                Being born as a foreigner and being from a low-income family household. I
+                wouldn&rsquo;t have dreamt of being in college and having an &ldquo;American
+                Dream&rdquo;, not until I&rsquo;ve realized that I can change that dream and
+                turn it into reality. When I was the age of three, I moved from the
+                Philippines to the US with my mother, my father had the money to bring us
+                here to the US as his side of the family was well off. But because my father
+                was the only one who was generating the income, it was hard for us to only
+                depend on him, so my mother took her time to find a job while also trying
+                her best to teach me some English words that she remembered learned from
+                school so that when I start school I would be at least ready to communicate
+                with other students and the teacher. As I started elementary school, I had a
+                hard time making friends and communicating with them due to a language
+                barrier and as kids, some would make fun of my accent and my inability of
+                speaking English, so they placed me into an ESL class and within time and
+                practice I&rsquo;ve gotten better with my English and understanding the
+                things around me. One day my parent gifted me my first device with a few
+                games, the console&nbsp;was called&nbsp;a Nintendo DS. As this was my first
+                console, the first thing I did to it was to fidget around with it and learn
+                how to make it work since I had no clue on how to use it, when I figured out
+                how to use it I was very excited. One day I was curious about how this
+                device worked so I played around with it until it broke, I was a little
+                saddened that my DS broke, but my father showed me that things could always
+                be fixed, so he demonstrated to me how to replace a battery on the DS. At
+                that moment that&rsquo;s when I caught on to learning how to fix things by
+                replacing the batteries off non-working devices like a remote.
+        </p>
+        <p class="statment_P">
+            &nbsp;
+        </p>
+        <p class="statment_P">
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; During my time in middle school,
+                I took a computer class that guided us on how to use the Windows operating
+                system, the internet, and how to secure yourself from malicious programs and
+                scammers. This class is what sparked my interest in how computers worked,
+                and because I liked fixing things, I&rsquo;ve always wondered how people
+                fixed things within the internet. So then, I came looking for answers, and
+                as you know the almighty google has shown me careers that&rsquo;s associated
+                with computers and the internet, as a kid I was fascinated with the word
+                &ldquo;hacker&rdquo; and I learned that cybersecurity was that hacker job
+                that fits my desired career. But as time goes on, I&rsquo;ve become more
+                familiar with how the Operating systems and internet works, and during my
+                spare time after school, I would try to learn how to code through google
+                chrome&rsquo;s developer tool. As I continue to practice and use this tool,
+                I become more interested in wanting to develop my own apps and software. And
+                so, I&rsquo;ve made it a goal after high school that I would become a
+                software developer. Not only that I&rsquo;ve always had it as a goal to make
+                my parents proud for their hard work, and I&rsquo;ve always wanted to repay
+                that hardship by showing them I will succeed and help make their lives
+                easier with my future career, as I plan on developing an app that would help
+                benefit them. This Is when I realized that my dream is becoming more of a
+                reality as I slowly step forward closer to it. There will be roadblocks
+                achieving this dream, but I know for a fact that It will be worth it at the
+                end.
+        </p>
+    </section>
+
+    <section class="skillContainer">
+        <h3 id="personalSkills">Personal Skills</h3>
+        <aside class="skill1">
+            <h4>Industry Knowledge</h4>
+            <ul>
+                <li>Education</li>
+                <li>Computer Science</li>
+                <li>Mathematics</li>
+                <li>Heavy Equipment</li>
+                <li>Science</li>
+            </ul>
+        </aside>
+        <aside class="skill2">
+            <h4>Interpersonal Skills</h4>
+            <ul>
+                <li>Teamwork</li>
+                <li>Customer Service</li>
+                <li>Communication</li>
+                <li>Teaching</li>
+                <li>Leadership</li>
+            </ul>
+        </aside>
+        <aside class="skill3">
+            <h4>Other Skills</h4>
+            <ul>
+                <li>Programming Languages</li>
+                <li>Food Service</li>
+                <li>Consumer Services</li>
+                <li>Calculus</li>
+            </ul>
+        </aside>
+    </section>
+
+    <section class="goalContainer">
+        <h3 id="personalGoals">Personal Goals</h3>
+        <aside class="goal1">
+            <h4>Education</h4>
+            <ul>
+                <li>Get my bachelors degree</li>
+                <li>learn more CS languages</li>
+                <li>Get better at math</li>
+                <li>Reach for a 4.0 GPA</li>
+            </ul>
+        </aside>
+        <aside class="goal2">
+            <h4>Bucket List</h4>
+            <ul>
+                <li>Go sky diving</li>
+                <li>Travel to many places</li>
+                <li>Hike to places with an scenic view</li>
+                <li></li>
+            </ul>
+        </aside>
+        <aside class="goal3">
+            <h4>Personal</h4>
+            <ul>
+                <li>Get an internship</li>
+                <li>Become a full stack developer</li>
+                <li>learn how to create a device with a Raspberry Pi</li>
+            </ul>
+        </aside>
+        <aside class="goal4">
+            <h4>Hobby</h4>
+            <ul>
+                <li>Finish building my race car</li>
+                <li>Finish building my offroad car</li>
+                <li>Build an ultimate computer</li>
+                <li></li>
+            </ul>
+        </aside>
+    </section>
+</aside>
+
+
+
+<?php
+// This puts the HTML for the footer into the page.
+include "includes/footer.php";
+?>
